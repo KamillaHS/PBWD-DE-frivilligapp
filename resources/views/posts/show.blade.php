2a88ba5c41eb2a05a6a2cf33940e84b43065010b
KamillaHS <kamilla.strobaek@gmail.com>
2018-10-29 12:05:53 +0100

    Edit and delete post added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,4 +11,10 @@
 
     <br><small>Written at {{$post->created_at}}</small>
 
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'style' => 'float:right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection

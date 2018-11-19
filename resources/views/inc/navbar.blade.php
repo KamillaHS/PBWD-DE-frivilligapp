00f4571bbfda2b4afe5f1001e496e5288c875a6b
KamillaHS <kamilla.strobaek@gmail.com>
2018-11-19 13:30:48 +0100

    Unessesary pages outcommented

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -25,9 +25,9 @@
 
             <ul class="nav navbar-nav">
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
+                {{--<li><a href="/about">About</a></li>--}}
                 <li><a href="/historik">Historik</a></li>
-                <li><a href="/posts">Blog</a></li>
+                {{--<li><a href="/posts">Blog</a></li>--}}
             </ul>
 
             <!-- Right Side Of Navbar -->
